33ba0d62b0b9ecc201afa85b22eb699254011c1d
blackops97 <exetra29988@gmail.com>
2016-07-23 03:34:34 +0300

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,5 @@
 do local _ = {
-  about_text = "Welcome to DevPoint For more information Subscribe to the channel @DevPointTeam",
+  about_text = "Welcome to DevPoint For more information Subscribe to the channel @sajad_iq98",
   enabled_plugins = {
          "admin",
     "onservice",
